1d3e503f2c30bc5e1cce8ca4037478cf0c232cce
Sarank11 <166124785+Sarank11@users.noreply.github.com>
2026-07-12 17:16:21 +0100

    Tweak greeting copy and seed London-flavoured starting data
    Greeting reads "Welcome back {name}" without italics, starting
    balance is 71832.79, and the default transaction history is now a
    set of London merchants (Harrods, Claridge's, TfL, Fortnum & Mason,
    Addison Lee, Selfridges, Borough Market) instead of generic ones.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "british-express-v1";
+const CACHE = "british-express-v2";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest",
   "./icons/icon-192.png", "./icons/icon-512.png", "./icons/apple-touch-icon.png"];
 
